37be9c327db590953bda4e2603b6f594a629898e
Hannah <hannahgrandine@gmail.com>
2020-04-12 15:59:35 -0700

    changed initial file and added init.py file to Level3

--- Level3/ClassroomQuiz.py
+++ Level3/ClassroomQuiz.py
@@ -1,85 +1,8 @@
-import pygame
-import random
-import time
-from os import path
-
-"""Initialize Font Objects"""
-#We pick our text style and size.
-pygame.init()
-myfont = pygame.font.SysFont('Arial', 35)
-answer_1_text = myfont.render("                        ", True, (0, 0, 0))
-answer_1_rect = answer_1_text.get_rect(topleft=(200,230))
-answer_2_text = myfont.render("                         ", True, (0, 0, 0))
-answer_2_rect = answer_2_text.get_rect(topleft=(200,300))
-answer_3_text = myfont.render("                         ", True, (0, 0, 0))
-answer_3_rect = answer_3_text.get_rect(topleft=(200,370))
-sample_click = myfont.render("Click here to start the game.", True, (0,0,0))
-sample_click_rect = sample_click.get_rect(topleft=(200,230))
-correct_text = myfont.render("That is correct.", True, (0,128,0))
-incorrect_text = myfont.render("That is incorrect.", True, (255,0,0))
-
-def get_file(fileName):
-    """Returns the absolute path of a file."""
-    #This grabs your files from your folder.
-    return path.join(path.dirname(__file__), fileName)
-
-def randomize_answers(answerChoices):
-    #This randomly shuffles the answers so the player doesn't know which one is the correct answer.
-    random.shuffle(answerChoices)
-    return answerChoices
-
-def display_question():
-    #This code loads and displays the next question and Mrs. Codala's reaction.
-    question_text = myfont.render(question, True, (0, 0, 0))
-    question_rect = question_text.get_rect(topleft=(200,150))
-    answer_1_text = myfont.render(ANSWER_CHOICES[0], True, (0, 0, 0))
-    answer_1_rect = answer_1_text.get_rect(topleft=(200,230))
-    answer_2_text = myfont.render(ANSWER_CHOICES[1], True, (0, 0, 0))
-    answer_2_rect = answer_2_text.get_rect(topleft=(200,300))
-    answer_3_text = myfont.render(ANSWER_CHOICES[2], True, (0, 0, 0))
-    answer_3_rect = answer_3_text.get_rect(topleft=(200,370))
-    screen.blit(background, (0,0))
-    screen.blit(question_text, question_rect)
-    screen.blit(answer_1_text, answer_1_rect)
-    screen.blit(answer_2_text, answer_2_rect)
-    screen.blit(answer_3_text, answer_3_rect)
-    pygame.display.update()
-
-def display_intro_screen():
-    #This shows the intro text (so we only run it once, at the beginning).
-    intro_text = myfont.render("Welcome to the Trivia Game!", True, (0,0,0))
-    sample_click = myfont.render("Click here to start.", True, (0,0,0))
-    sample_click_rect = sample_click.get_rect(topleft=(200,230))
-    screen.blit(background,(0,0))
-    screen.blit(intro_text, (200,150))
-    screen.blit(sample_click,sample_click_rect)
-    pygame.display.update()
-
-def display_end_screen():
-    #This shows the image of Mrs. Codala telling you the game is done.
-    kodala = end_game
-    screen.blit(background, (0,0))
-    screen.blit(kodala,(0,0))
-    pygame.display.update()
-
-"""Load Sprites"""
-#We load the images and put them in variables.
-background = pygame.image.load(get_file('Background.png'))
-correct_a = pygame.image.load(get_file('CorrectAnswerA.png'))
-correct_b = pygame.image.load(get_file('CorrectAnswerB.png'))
-incorrect_a = pygame.image.load(get_file('IncorrectAnswerA.png'))
-incorrect_b = pygame.image.load(get_file('IncorrectAnswerB.png'))
-end_game = pygame.image.load(get_file('EndGame.png'))
-
-"""Set Window Size"""
-#We set the window size for our game.
-width = 960
-height = 540
-screen = pygame.display.set_mode((width,height))
+from init import *
 
 #We pull each line of text from the file into a list.
 TRIVIA = []
-file = open(get_file('trivia.txt'), 'r')
+file = open(get_file('Assets/trivia.txt'), 'r')
 for line in file:
     TRIVIA.append(line.rstrip())
 
@@ -107,7 +30,7 @@ while running == False:
             if sample_click_rect.collidepoint(mpos):
                 running = True
 while running:
-    display_question() #Displays the new question and the three answer choices.
+    display_question(question, ANSWER_CHOICES) #Displays the new question and the three answer choices.
     EVENTS = pygame.event.get()
     for event in EVENTS:
         mpos = pygame.mouse.get_pos() 
