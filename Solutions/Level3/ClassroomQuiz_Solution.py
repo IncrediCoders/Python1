b9262e20e34654cd04c1b46eddbc9892b85d7077
Irena Niu <irenaniu@gmail.com>
2020-09-10 21:30:11 -0700

    Simplified the running loop

--- Solutions/Level3/ClassroomQuiz_Solution.py
+++ Solutions/Level3/ClassroomQuiz_Solution.py
@@ -43,32 +43,27 @@ while running:
             running = False
         mouse_position = pygame.mouse.get_pos() 
         if event.type == pygame.MOUSEBUTTONDOWN: #If the player clicks the mouse.
-            if answer_1_rect.collidepoint(mouse_position): #If the player clicks the first answer on the top.
+            if answer_1_rect.collidepoint(mouse_position): #If the player clicks the 1st answer on the top.
                 if ANSWER_CHOICES[0] == answer: #If it's the correct answer.
                     display_codala(correct_a, "correct_text") #Displays codala and text for correct answer.
                 else: #If it's an incorrect answer.
                     display_codala(incorrect_a, "incorrect_text") #Display codala and text for incorrect answer.
-                #Checks if it's the last question and display end screen.
-                running = check_if_last_question(line_number, running, number_of_questions)
-                #If it's the not the last question, we display the next question.
-                line_number, question, answer, ANSWER_CHOICES = move_to_next_question(TRIVIA, 
-                                                                question, line_number, ANSWER_CHOICES)
             if answer_2_rect.collidepoint(mouse_position): #If the player clicks the 2nd answer.
                 if ANSWER_CHOICES[1] == answer: 
                     display_codala(correct_b, "correct_text")
                 else:
                     display_codala(incorrect_b, "incorrect_text")
-                running = check_if_last_question(line_number, running, number_of_questions)
-                line_number, question, answer, ANSWER_CHOICES = move_to_next_question(TRIVIA, 
-                                                                question, line_number, ANSWER_CHOICES)
+
 # TODO:Write the code here for the third answer.
             if answer_3_rect.collidepoint(mouse_position): #If the player clicks the 3rd answer.
                 if ANSWER_CHOICES[2] == answer: 
                     display_codala(correct_a, "correct_text")
                 else:
                     display_codala(incorrect_a, "incorrect_text")
-                running = check_if_last_question(line_number, running, number_of_questions)
-                line_number, question, answer, ANSWER_CHOICES = move_to_next_question(TRIVIA, 
-                                                                question, line_number, ANSWER_CHOICES)
 
+            #Checks if it's the last question and display end screen.
+            running = check_if_last_question(line_number, running, number_of_questions)
+            #If it's the not the last question, we display the next question.
+            line_number, question, answer, ANSWER_CHOICES = move_to_next_question(TRIVIA, 
+                                                            question, line_number, ANSWER_CHOICES)
 pygame.quit()
