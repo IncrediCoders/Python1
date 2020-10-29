c5a6f74b18ecd6bcf63feb234f14e1dcd652df1e
Ed Price - MSFT <edprice@microsoft.com>
2020-10-29 15:45:48 -0700

    Update TurtleMap.py

--- Level1/TurtleMap.py
+++ Level1/TurtleMap.py
@@ -1,30 +1,30 @@
-"""Turtle graphics project file."""
+#Turtle graphics project file
 from init import *
 import turtle
 
-# Create our screen
+#Create our screen
 SCREEN = turtle.Screen()
 SCREEN.title("Mapster Spacebook")
 SCREEN.setup(1280, 800)
 SCREEN.bgpic(get_file("Assets/Background.gif"))
 
-# Set the shape to be a green turtle.
+#Set the shape to be a green turtle
 turtle.shape("turtle")
 turtle.color("green") 
 
-# This code moves the turtle to its starting location.
+#This code moves the turtle to its starting location
 turtle.penup()
 turtle.setx(460)
 turtle.sety(-275)
 turtle.pendown()
 
-#TODO: Uncomment these lines by removing the hashtags.
+#TODO: Uncomment these lines by removing the hashtags
 #turtle.left(180)
 #turtle.forward(510)
 
-#TODO: Copy the two lines Grafika gives you here.
+#TODO: Copy the two lines Grafika gives you here
 
-#TODO: Write the last 4 lines, following Grafika's instructions.
+#TODO: Write the last 4 lines, following Grafika's instructions
 
-# This line stops the window from closing once we make it to the end.
+#This line stops the window from closing, once we make it to the end
 turtle.done()
