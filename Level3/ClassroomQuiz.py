bd365446298880fc70d13c06564926da8e31e76a
Hannah <hannah@grandine.org>
2020-12-03 13:44:03 -0800

    shortened long lines and removed trailing spaces

--- Level3/ClassroomQuiz.py
+++ Level3/ClassroomQuiz.py
@@ -3,8 +3,8 @@ from init import *
 #We pull each line of text from the file into a list
 TRIVIA = read_file('Assets/Trivia.txt')
 
-#We create a list of variables that holds the text for our current question, the correct answer, and the two wrong choices, in that order
-#We use this list to display questions and answers on the screen
+#We create a list of variables that holds the text for our current question, the correct answer, and
+#the two wrong choices, in that order. We use this list to display text on the screen
 question = TRIVIA[0]
 answer = TRIVIA[1]
 wrong_choice_1 = TRIVIA[2]
@@ -18,7 +18,7 @@ number_of_questions = 8
 
 display_intro_screen() #Displays the intro screen
 
-running = True 
+running = True
 
 #This displays the question screen until the last question or until the player closes the window
 while running:
@@ -27,7 +27,7 @@ while running:
     for event in EVENTS:
         if event.type == pygame.QUIT: #If the player clicks the Close button, it exits the game
             running = False
-        mouse_position = pygame.mouse.get_pos() 
+        mouse_position = pygame.mouse.get_pos()
         if event.type == pygame.MOUSEBUTTONDOWN: #If the player clicks the mouse
             if answer_1_rect.collidepoint(mouse_position): #If the player clicks the 1st answer on the top
                 if ANSWER_CHOICES[0] == answer: #If it's the correct answer
@@ -35,7 +35,7 @@ while running:
                 else: #If it's an incorrect answer
                     display_codala(incorrect_a, "incorrect_text") #Displays Mrs. Codala and the text for an incorrect answer
             #if answer_2_rect.collidepoint(mouse_position): #If the player clicks the 2nd answer
-                #if ANSWER_CHOICES[1] == answer: 
+                #if ANSWER_CHOICES[1] == answer:
                     #display_codala(correct_b, "correct_text")
                 #else:
                     #display_codala(incorrect_b, "incorrect_text")
