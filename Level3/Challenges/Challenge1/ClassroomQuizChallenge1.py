235a5fc3314e6cdc839f32896c5218fadf08b6a2
Ed Price - MSFT <edprice@microsoft.com>
2021-02-05 12:24:29 -0800

    Update ClassroomQuizChallenge1.py
    Updated the comments

--- Level3/Challenges/Challenge1/ClassroomQuizChallenge1.py
+++ Level3/Challenges/Challenge1/ClassroomQuizChallenge1.py
@@ -15,8 +15,8 @@ randomize_answers(ANSWER_CHOICES) #Shuffles the first set of answer choices
 
 line_number = 0
 number_of_questions = 12
-#TODO: Uncomment this line to add the variable of score
-#score = 0 #Records the correct answering
+#TODO: Uncomment this line to add the score variable
+#score = 0 #Records the current number of correct answers
 
 display_intro_screen() #Displays the intro screen
 
@@ -27,33 +27,33 @@ while running:
     display_question(question, ANSWER_CHOICES) #Displays the new question and the three answer choices
     EVENTS = pygame.event.get()
     for event in EVENTS:
-        if event.type == pygame.QUIT: #If clicks the close button, it exits the game
+        if event.type == pygame.QUIT: #If the player clicks the close button, it exits the game
             running = False
         mouse_position = pygame.mouse.get_pos()
         if event.type == pygame.MOUSEBUTTONDOWN: #If the player clicks the mouse
             if answer_1_rect.collidepoint(mouse_position): #If the player clicks the 1st answer on the top
                 if ANSWER_CHOICES[0] == answer: #If it's the correct answer
-                    #TODO: Uncomment this line to add 1 score
-                    #score += 1 #The player gets one score
-                    display_codala(correct_a, "correct_text") #Displays codala and text for correct answer
+                    #TODO: Uncomment this line to add 1 to the score
+                    #score += 1 #The player gets another correct answer
+                    display_codala(correct_a, "correct_text") #Displays Mrs. Codala and the text for a correct answer
                 else: #If it's an incorrect answer
-                    display_codala(incorrect_a, "incorrect_text") #Display codala and text for incorrect answer
+                    display_codala(incorrect_a, "incorrect_text") #Displays Mrs. Codala and the text for an incorrect answer
             if answer_2_rect.collidepoint(mouse_position): #If the player clicks the 2nd answer
                 if ANSWER_CHOICES[1] == answer:
-                    #TODO: Add 1 score
+                    #TODO: Add 1 to the score
 
                     display_codala(correct_b, "correct_text")
                 else:
                     display_codala(incorrect_b, "incorrect_text")
             if answer_3_rect.collidepoint(mouse_position): #If the player clicks the 3rd answer
                 if ANSWER_CHOICES[2] == answer:
-                    #TODO: Add 1 score
+                    #TODO: Add 1 to the score
 
                     display_codala(correct_a, "correct_text")
                 else:
                     display_codala(incorrect_a, "incorrect_text")
 
-            #TODO: Check if it's the last question and display end screen with scores
+            #TODO: Check if it's the last question and display the end screen with the scores
             #Modified from the check_if_last_question function in init.py
             
             
