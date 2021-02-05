c77d9f3f723ae3a34f001977660a9734a53ded29
Ed Price - MSFT <edprice@microsoft.com>
2021-02-05 12:36:45 -0800

    Update ClassroomQuizChallenge2_Solution.py
    Updated the comments

--- Solutions/Level3/ClassroomQuizChallenge2_Solution.py
+++ Solutions/Level3/ClassroomQuizChallenge2_Solution.py
@@ -3,8 +3,8 @@ from init import *
 #We pull each line of text from the file into a list
 TRIVIA = read_file('Assets/Trivia.txt')
 
-#We now have a list that holds our current question, correct answer, and the two wrong choices, in that order
-#We use these variables to display the text on the screen
+#We create a list of variables that holds the text for our current question, the correct answer, and
+#the two wrong choices, in that order. We use this list to display text on the screen
 question = TRIVIA[0]
 answer = TRIVIA[1]
 wrong_choice_1 = TRIVIA[2]
@@ -25,33 +25,33 @@ while running:
     display_question(question, ANSWER_CHOICES) #Displays the new question and the three answer choices
     EVENTS = pygame.event.get()
     for event in EVENTS:
-        if event.type == pygame.QUIT: #If clicks the close button, it exits the game
+        if event.type == pygame.QUIT: #If the player clicks the Close button, it exits the game
             running = False
         mouse_position = pygame.mouse.get_pos() 
         if event.type == pygame.MOUSEBUTTONDOWN: #If the player clicks the mouse
             if answer_1_rect.collidepoint(mouse_position): #If the player clicks the 1st answer on the top
                 if ANSWER_CHOICES[0] == answer: #If it's the correct answer
-                    #Uncommented these lines to add interactive effect
-                    #Change color from blue to green
+                    #Uncommented these lines to add an interactive effect
+                    #Changes the text color from blue to green
                     answer_1_text = my_font.render(answer, True, (0,128,0))
                     screen.blit(answer_1_text, answer_1_rect)
                     pygame.display.update()
                     pygame.event.get()
                     time.sleep(1)
-                    display_codala(correct_a, "correct_text") #Displays codala and text for correct answer
+                    display_codala(correct_a, "correct_text") #Displays Mrs. Codala and the text for a correct answer
                 else: #If it's an incorrect answer
-                    #Add interactive effect
-                    #Change color from blue to red
+                    #Added an interactive effect to the text
+                    #Changes the text color from blue to red
                     answer_1_text = my_font.render(ANSWER_CHOICES[0], True, (255,0,0))
                     screen.blit(answer_1_text, answer_1_rect)
                     pygame.display.update()
                     pygame.event.get()
                     time.sleep(1)
-                    display_codala(incorrect_a, "incorrect_text") #Display codala and text for incorrect answer
+                    display_codala(incorrect_a, "incorrect_text") #Display Mrs. Codala and the text for an incorrect answer
             if answer_2_rect.collidepoint(mouse_position): #If the player clicks the 2nd answer
                 if ANSWER_CHOICES[1] == answer: 
-                    #Add interactive effect
-                    #Change color from blue to green
+                    #Added an interactive effect to the text
+                    #Changes the text color from blue to green
                     answer_2_text = my_font.render(answer, True, (0,128,0))
                     screen.blit(answer_2_text, answer_2_rect)
                     pygame.display.update()
@@ -59,8 +59,8 @@ while running:
                     time.sleep(1)
                     display_codala(correct_b, "correct_text")
                 else:
-                    #Add interactive effect
-                    #Change color from blue to red
+                    #Added an interactive effect to the text
+                    #Changes the text color from blue to red
                     answer_2_text = my_font.render(ANSWER_CHOICES[1], True, (255,0,0))
                     screen.blit(answer_2_text, answer_2_rect)
                     pygame.display.update()
@@ -69,8 +69,8 @@ while running:
                     display_codala(incorrect_b, "incorrect_text")
             if answer_3_rect.collidepoint(mouse_position): #If the player clicks the 3rd answer
                 if ANSWER_CHOICES[2] == answer: 
-                    #Add interactive effect
-                    #Change color from blue to green
+                    #Added an interactive effect to the text
+                    #Changes the text color from blue to green
                     answer_3_text = my_font.render(answer, True, (0,128,0))
                     screen.blit(answer_3_text, answer_3_rect)
                     pygame.display.update()
@@ -78,8 +78,8 @@ while running:
                     time.sleep(1)
                     display_codala(correct_a, "correct_text")
                 else:
-                    #Add interactive effect
-                    #Change color from blue to red
+                    #Added an interactive effect to the text
+                    #Changes the text color from blue to red
                     answer_3_text = my_font.render(ANSWER_CHOICES[2], True, (255,0,0))
                     screen.blit(answer_3_text, answer_3_rect)
                     pygame.display.update()
@@ -87,7 +87,7 @@ while running:
                     time.sleep(1)
                     display_codala(incorrect_a, "incorrect_text")
 
-            #Checks if it's the last question and display end screen
+            #Checks if it's the last question, and then displays the end screen
             running = check_if_last_question(line_number, running, number_of_questions)
             #If it's the not the last question, we display the next question
             line_number, question, answer, ANSWER_CHOICES = move_to_next_question(TRIVIA, question, line_number, ANSWER_CHOICES)
