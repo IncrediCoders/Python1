2d372f58e4a6969529916b50d923977f07e96720
Ed Price - MSFT <edprice@microsoft.com>
2021-02-05 12:24:33 -0800

    Update ClassroomQuizChallenge1_Solution.py
    Updated the comments

--- Solutions/Level3/ClassroomQuizChallenge1_Solution.py
+++ Solutions/Level3/ClassroomQuizChallenge1_Solution.py
@@ -3,8 +3,8 @@ from init import *
 #We pull each line of text from the file into a list
 TRIVIA = read_file('Assets/Trivia.txt')
 
-#We now have a list that holds our current question, correct answer, and the two wrong choices, in that order
-#We use these variables to display the text on the screen
+#We create a list of variables that holds the text for our current question, the correct answer, and
+#the two wrong choices, in that order. We use this list to display text on the screen
 question = TRIVIA[0]
 answer = TRIVIA[1]
 wrong_choice_1 = TRIVIA[2]
@@ -15,8 +15,8 @@ randomize_answers(ANSWER_CHOICES) #Shuffles the first set of answer choices
 
 line_number = 0
 number_of_questions = 12
-#Uncommented this line to add the variable of score
-score = 0 #Records the correct answering
+#Uncommented this line to add the score variable
+score = 0 #Records the current number of correct answers
 
 display_intro_screen() #Displays the intro screen
 
@@ -27,37 +27,37 @@ while running:
     display_question(question, ANSWER_CHOICES) #Displays the new question and the three answer choices
     EVENTS = pygame.event.get()
     for event in EVENTS:
-        if event.type == pygame.QUIT: #If clicks the close button, it exits the game
+        if event.type == pygame.QUIT: #If the player clicks the close button, it exits the game
             running = False
         mouse_position = pygame.mouse.get_pos() 
         if event.type == pygame.MOUSEBUTTONDOWN: #If the player clicks the mouse
             if answer_1_rect.collidepoint(mouse_position): #If the player clicks the 1st answer on the top
                 if ANSWER_CHOICES[0] == answer: #If it's the correct answer
-                    #Uncommented this line to add 1 score
-                    score += 1 #The player gets one score
-                    display_codala(correct_a, "correct_text") #Displays codala and text for correct answer
+                    #Uncommented this line to add 1 to the score
+                    score += 1 #The player gets another correct answer
+                    display_codala(correct_a, "correct_text") #Displays Mrs. Codala and the text for a correct answer
                 else: #If it's an incorrect answer
-                    display_codala(incorrect_a, "incorrect_text") #Display codala and text for incorrect answer
+                    display_codala(incorrect_a, "incorrect_text") #Displays Mrs. Codala and the text for an incorrect answer
             if answer_2_rect.collidepoint(mouse_position): #If the player clicks the 2nd answer
                 if ANSWER_CHOICES[1] == answer: 
-                    #Added this line to add 1 score
+                    #Added this line to add 1 to the score
                     score += 1
                     display_codala(correct_b, "correct_text")
                 else:
                     display_codala(incorrect_b, "incorrect_text")
             if answer_3_rect.collidepoint(mouse_position): #If the player clicks the 3rd answer
                 if ANSWER_CHOICES[2] == answer: 
-                    #Added this line to add 1 score
+                    #Added this line to add 1 to the score
                     score += 1
                     display_codala(correct_a, "correct_text")
                 else:
                     display_codala(incorrect_a, "incorrect_text")
 
-            #Check if it's the last question and display end screen with scores
+            #Checks if it's the last question and displays the end screen with the scores
             #Modified from the check_if_last_question function in init.py
             if line_number >= (number_of_questions * 4)-4:
                 display_end_screen()
-                #Display the final score on the end screen
+                #Displays the final score on the end screen
                 if score == number_of_questions:
                     score_text = my_font.render("Congratulations! You got all questions right! " + str(score) + " / " + str(number_of_questions), True, (0,128,0))
                 elif score == 0:
