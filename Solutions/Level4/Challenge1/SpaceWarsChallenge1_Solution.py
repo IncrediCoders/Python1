9eb8bafac7a28266dd1ccbd15bed85e60c7a4641
kmiller1197 <55461331+kmiller1197@users.noreply.github.com>
2021-04-16 12:28:20 -0700

    Update SpaceWarsChallenge1_Solution.py
    Changed format of comments to match other challenges and removed import sys and import random

--- Solutions/Level4/Challenge1/SpaceWarsChallenge1_Solution.py
+++ Solutions/Level4/Challenge1/SpaceWarsChallenge1_Solution.py
@@ -1,11 +1,9 @@
-"""Runs the Init.py file and imports the libraries"""
+#Runs the Init.py file and imports the libraries
 from init import *
 import pygame
-import random
-import sys
 
+#The Update method checks for all the key presses and button clicks
 def update(delta_time):
-    """The Update method checks for all the key presses and button clicks"""
     for event in pygame.event.get():
         #Checks if you click the Replay button to play again
         check_replay_click(event)
@@ -46,19 +44,19 @@ def update(delta_time):
     elif key_held_down(pygame.K_DOWN):
         MY.player2.add_velocity(MY.player2.rotation, -ship_accel, ship_max_speed)
     
-    # Updates player objects on screen
+    #Updates player objects on screen
     update_players(delta_time)
 
-    # Checks if bullets have been fired and updates their behavior on screen
+    #Checks if bullets have been fired and updates their behavior on screen
     update_bullets(delta_time)
 
     # Check win condition
     check_win()   
 
 
-# states
+#states
 Manager.register(sys.modules[__name__]) #The current file
 Manager.register(GameOver)
 
-# run the game!
+#run the game!
 Manager.run(SCREEN, WINDOW, BLACK)
