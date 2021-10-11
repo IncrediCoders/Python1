a77280272b06b7ce9eb64f4770cd2191c38f78ae
Ed Price - MSFT <edprice@microsoft.com>
2021-10-11 14:20:28 -0700

    Update SpaceWars_Solution.py
    Updated the comments to remove the "TODO" parts.

--- Solutions/Level4/SpaceWars_Solution.py
+++ Solutions/Level4/SpaceWars_Solution.py
@@ -18,26 +18,26 @@ def update(delta_time):
     #Rotates the Player 1 ship
     if key_held_down(pygame.K_a):
         MY.player1.add_rotation(ship_rotate * delta_time)
-    #TODO: Copy the code here for the Player 1 ship to rotate clockwise
+    #Copied the code here for the Player 1 ship to rotate clockwise
     elif key_held_down(pygame.K_d):
         MY.player1.add_rotation(-ship_rotate * delta_time)
 
     #Moves the Player 1 ship forward and backward
     if key_held_down(pygame.K_w):
         MY.player1.add_velocity(MY.player1.rotation, ship_accel, ship_max_speed)
-    #TODO: Copy the code here for the Player 1 ship to move backward
+    #Copied the code here for the Player 1 ship to move backward
     elif key_held_down(pygame.K_s):
         MY.player1.add_velocity(MY.player1.rotation, -ship_accel, ship_max_speed)
 
     #Rotates the Player 2 ship
-    #TODO: Write the code here to rotate the Player 2 ship
+    #Wrote the code here to rotate the Player 2 ship
     if key_held_down(pygame.K_LEFT):
         MY.player2.add_rotation(ship_rotate * delta_time)
     elif key_held_down(pygame.K_RIGHT):
         MY.player2.add_rotation(-ship_rotate * delta_time)
 
     #Moves the Player 2 ship forward and backward
-    #TODO: Write the code here to move the Player 2 ship
+    #Wrote the code here to move the Player 2 ship
     if key_held_down(pygame.K_UP):
         MY.player2.add_velocity(MY.player2.rotation, ship_accel, ship_max_speed)
     elif key_held_down(pygame.K_DOWN):
