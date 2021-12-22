49a9d1666c317a3a84fb2c6bf1cdd454af9c4664
Ed Price - MSFT <edprice@microsoft.com>
2021-12-22 14:02:08 -0800

    Update IncrediCardsChallenge1.py
    Updated Code Comments

--- Level7/Challenges/Challenge1/IncrediCardsChallenge1.py
+++ Level7/Challenges/Challenge1/IncrediCardsChallenge1.py
@@ -71,14 +71,14 @@ class PlayScreen(GameState):
 		self.attacker = self.player1 if self.player1.active_turn else self.player2
 		self.defender = self.player1 if not self.player1.active_turn else self.player2
 		
-		# TODO: Update the card display data for both players (Hint: look at lines 585-624 in the init.py file)
+		#TODO: Update the card display data for both players (Hint: look at lines 585-624 in the init.py file)
 		self.player1_card_display = CardDisplay(self.player1, player1_card_display_data) #change to player1_card_display_data_challenge
 		self.player2_card_display = CardDisplay(self.player2, player2_card_display_data) 
 
 		self.player1_healthbar = Healthbar(self.player1, player1_healthbar_data)
 		self.player2_healthbar = Healthbar(self.player2, player2_healthbar_data)
 		
-		# TODO: Update the on deck data for both players (Hint: lookat lines 626-677 in the init.py file)
+		#TODO: Update the on deck data for both players (Hint: lookat lines 626-677 in the init.py file)
 		self.player1_ondeck = OnDeck(self.player1, player1_ondeck_data) #change to player1_ondeck_data_challenge
 		self.player2_ondeck = OnDeck(self.player2, player2_ondeck_data) 
 
@@ -202,7 +202,7 @@ if __name__=="__main__":
 			"Title" : TitleScreen(),
 			"GetNames": GetNameScreen(),
 			"CoinFlip": CoinFlipScreen(),
-			# TODO: Add a second argument (boolean True) to the ChooseHandScreen parameters
+			#TODO: Add a second argument (boolean True) to the ChooseHandScreen parameters
 			"ChooseHand" : ChooseHandScreen(DECK), 
 			"Game" : PlayScreen(),
 			"Victory" : VictoryScreen()
