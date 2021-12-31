0a1293b47a5c6a63458e86c0ae22ac91a31d4244
Madeline Ambrose <ambrose907@gmail.com>
2021-12-31 10:58:31 -0800

    Match Level 5 template with solution

--- Level5/CreeperChase.py
+++ Level5/CreeperChase.py
@@ -13,9 +13,9 @@ def update(delta_time):
         #TODO: Uncomment the two lines below
         # MY.player.velocity.x = max(MY.player.velocity.x - PLAYER_ACCEL, -PLAYER_MAX_SPEED)
         # MY.player.sprite = MY.paul_run_left
-    #TODO: Copy the elif statement here for the player's righthand movement
+    #TODO: Copy the elif statement here for the Paul's righthand movement
 
-        #TODO: Copy the code here to set the player's velocity 
+        #TODO: Copy the code here to set the Paul's velocity 
 
         #TODO: Write the code here to set Paul's righthand movement animation
 
@@ -49,16 +49,15 @@ def update(delta_time):
     #Check for hazard collisions
     for hazard in MY.hazards:
         if MY.player.collides_with(hazard):
-            MY.player_health -= 2
-            if MY.player_health <= 0:
+            MY.player_health -= 1
+            if MY.player_health == 0:
                 change(1)
             else:
                 MY.player.location = MY.player_start_position
                 MY.player.set_velocity(0, 0)
                 MY.player.sprite = MY.paul_pain_right
-                break
     
-    #Update the player's location
+    #Update the Paul's location
     MY.player.update(delta_time)
 
     #Check for wall collisions
@@ -69,22 +68,18 @@ def update(delta_time):
                 MY.player.snap_to_object_y(wall, DOWN)
                 MY.player.velocity.y = 0
                 MY.grounded = touching = True
-                continue
             if MY.player.collision[LEFT]:
                 MY.player.snap_to_object_x(wall, LEFT)
                 MY.player.velocity.x = 0
                 touching = True
-                continue
             if MY.player.collision[RIGHT]:
                 MY.player.snap_to_object_x(wall, RIGHT)
                 MY.player.velocity.x = 0
                 touching = True
-                continue
             if MY.player.collision[UP]:
                 MY.player.snap_to_object_y(wall, UP)
                 MY.player.velocity.y = 0
                 touching = True
-                continue
     if not touching:
         MY.grounded = False
     
