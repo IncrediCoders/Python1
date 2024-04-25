15d1a901360b00ce0bdf76b4baeb4e78af9a733c
Gerry Wang <gerry.wang@hawxservices.com>
2024-04-25 15:05:48 -0700

    Made init level template to solution consistent

--- Solutions/Level5/init.py
+++ Solutions/Level5/init.py
@@ -845,8 +845,8 @@ def draw_timer():
         path = os.path.dirname(__file__) + '/Assets/Lilliput-steps.regular.ttf'
         font = pygame.font.Font(path, 32)
     else:
-       path = os.path.dirname(__file__) + '/Assets/Lilliput-steps.regular.ttf'
-       font = pygame.font.Font(path, 35) 
+        path = os.path.dirname(__file__) + '/Assets/Lilliput-steps.regular.ttf'
+        font = pygame.font.Font(path, 35) 
 
     time_remaining = 'TIME:' + str(MY.timer).split('.')[0]
     MY.text = font.render(time_remaining, True, (255, 0, 0))
