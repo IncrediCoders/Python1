015a234557491691d8d76aa50cd0ebb9ce3e500d
Gerry Wang <gerry.wang@hawxservices.com>
2024-08-29 11:32:04 -0700

    Added changes to level 3 challenge 2 codala display at end

--- Level3/Challenges/Challenge2/ClassroomQuiz_Challenge2.py
+++ Level3/Challenges/Challenge2/ClassroomQuiz_Challenge2.py
@@ -29,67 +29,68 @@ while running:
             running = False
         mouse_position = pygame.mouse.get_pos()
         if event.type == pygame.MOUSEBUTTONDOWN: # If the player clicks the mouse
-            if answer_1_rect.collidepoint(mouse_position): # If the player clicks the 1st answer on the top
-                if ANSWER_CHOICES[0] == answer: # If it's the correct answer
-                    #TODO: Uncomment these lines to add interactive effect
-                    ##Change color from blue to green
-                    #answer_1_text = my_font.render(answer, True, (0,128,0))
-                    #screen.blit(answer_1_text, answer_1_rect)
-                    #pygame.display.update()
-                    #pygame.event.get()
-                    #time.sleep(1)
-                    display_codala(correct_a, "correct_text") # Displays codala and text for correct answer
-                else: # If it's an incorrect answer
-                    #TODO: Add interactive effect
-                    # Change color from blue to red
-                     
-                     
+            # Check to see if player has clicked on one of the possible answers
+            if answer_1_rect.collidepoint(mouse_position) or answer_2_rect.collidepoint(mouse_position) or answer_3_rect.collidepoint(mouse_position):
+                if answer_1_rect.collidepoint(mouse_position): # If the player clicks the 1st answer on the top
+                    if ANSWER_CHOICES[0] == answer: # If it's the correct answer
+                        #TODO: Uncomment these lines to add interactive effect
+                        ##Change color from blue to green
+                        #answer_1_text = my_font.render(answer, True, (0,128,0))
+                        #screen.blit(answer_1_text, answer_1_rect)
+                        #pygame.display.update()
+                        #pygame.event.get()
+                        #time.sleep(1)
+                        display_codala(correct_a, "correct_text") # Displays codala and text for correct answer
+                    else: # If it's an incorrect answer
+                        #TODO: Add interactive effect
+                        # Change color from blue to red
+                        
+                        
+                        
                     
-                   
-                   
-                    display_codala(incorrect_a, "incorrect_text") # Display codala and text for incorrect answer
-            if answer_2_rect.collidepoint(mouse_position): # If the player clicks the 2nd answer
-                if ANSWER_CHOICES[1] == answer:
-                    #TODO: Add interactive effect
-                    # Change color from blue to green
-                     
+                    
+                        display_codala(incorrect_a, "incorrect_text") # Display codala and text for incorrect answer
+                if answer_2_rect.collidepoint(mouse_position): # If the player clicks the 2nd answer
+                    if ANSWER_CHOICES[1] == answer:
+                        #TODO: Add interactive effect
+                        # Change color from blue to green
+                        
+                    
+                    
                 
-                  
-              
-            
-                    display_codala(correct_b, "correct_text")
-                else:
-                    #TODO: Add interactive effect
-                    # Change color from blue to red
-                   
-                     
+                
+                        display_codala(correct_b, "correct_text")
+                    else:
+                        #TODO: Add interactive effect
+                        # Change color from blue to red
+                    
+                        
+                        
+                        
+                    
+                        display_codala(incorrect_b, "incorrect_text")
+                if answer_3_rect.collidepoint(mouse_position): # If the player clicks the 3rd answer
+                    if ANSWER_CHOICES[2] == answer:
+                        #TODO: Add interactive effect
+                        # Change color from blue to green
+                        
+                        
+                        
+                        
+                        
+                        display_codala(correct_a, "correct_text")
+                    else:
+                        #TODO: Add interactive effect
+                        # Change color from blue to red
+                        
+                        
                     
                     
-                   
-                    display_codala(incorrect_b, "incorrect_text")
-            if answer_3_rect.collidepoint(mouse_position): # If the player clicks the 3rd answer
-                if ANSWER_CHOICES[2] == answer:
-                    #TODO: Add interactive effect
-                    # Change color from blue to green
                     
-                     
-                     
-                     
-                     
-                    display_codala(correct_a, "correct_text")
-                else:
-                    #TODO: Add interactive effect
-                    # Change color from blue to red
-                     
-                    
-                  
-                  
-                   
-                    display_codala(incorrect_a, "incorrect_text")
-
+                        display_codala(incorrect_a, "incorrect_text")
+                # If it's the not the last question, we display the next question
+                line_number, question, answer, ANSWER_CHOICES = move_to_next_question(TRIVIA, question, line_number, ANSWER_CHOICES)
             # Checks if it's the last question and display end screen
             running = check_if_last_question(line_number, running, number_of_questions)
-            # If it's the not the last question, we display the next question
-            if running:
-                line_number, question, answer, ANSWER_CHOICES = move_to_next_question(TRIVIA, question, line_number, ANSWER_CHOICES)
-display_end_screen()
+            if not running:
+                display_end_screen()
