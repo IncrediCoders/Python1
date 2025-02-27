ab1f56cefee9e0b6ea5626f6ab775ed3766bfda1
Gerry Wang <gerry.wang@hawxservices.com>
2025-02-27 13:15:14 -0800

    Fixed level 2 challenge 1 and sounds for Level 4 Challenge 1 and 2

--- Level3/Challenges/Challenge1/ClassroomQuiz_Challenge1.py
+++ Level3/Challenges/Challenge1/ClassroomQuiz_Challenge1.py
@@ -72,7 +72,8 @@ while running:
             
             
             # We display the next question after an incorrect or correct answer has been chosen
-            line_number, question, answer, ANSWER_CHOICES = move_to_next_question(TRIVIA, question, line_number, ANSWER_CHOICES)
+            if running:
+                line_number, question, answer, ANSWER_CHOICES = move_to_next_question(TRIVIA, question, line_number, ANSWER_CHOICES)
             # If it's the last question, we display the end screen with Mrs. Codala
-            if not running:
+            else:
                 display_end_screen()
