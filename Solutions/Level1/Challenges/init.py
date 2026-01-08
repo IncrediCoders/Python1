1e6777cd9b8e331c877912d0128e0c1cedee75c4
Gerry Wang <gerry.wang@hawxservices.com>
2026-01-08 12:03:17 -0800

    Removed extraneous build and dist folders from Solutions files

--- Solutions/Level1/Challenges/init.py
+++
@@ -1,8 +0,0 @@
-from os import path
-
-def get_file(fileName):
-    """
-    Returns the absolute path of a file
-    """
-    # This grabs your files from your folder
-    return path.join(path.dirname(__file__), fileName)
