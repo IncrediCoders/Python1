dff1dd7064c9abe9129a6f7fe5b02ad602d83cbe
gerrywang <gerry.wang@hawxservices.com>
2026-02-27 10:15:19 -0800

    Added a space into Level 7 line 68 betwen hash and comment

--- Solutions/Level7/IncrediCards_Solution.py
+++ Solutions/Level7/IncrediCards_Solution.py
@@ -65,7 +65,7 @@ class PlayScreen(GameState):
 		self.side_up = None
 		
 	def start(self, players):
-		#Uncommented lines 69-85 to setup player start values
+		# Uncommented lines 69-85 to setup player start values
 		self.players = players
 		self.player1 = players["player1"]
 		self.player2 = players["player2"]
