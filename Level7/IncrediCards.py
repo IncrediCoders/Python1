9aa59550e89134f35116580ab931bf1a7710fa6d
Ed Price <13895622+Ed-Price@users.noreply.github.com>
2026-02-27 10:17:31 -0800

    Update TODO comment for player start values setup
    Minor changes on the comment

--- Level7/IncrediCards.py
+++ Level7/IncrediCards.py
@@ -65,7 +65,7 @@ class PlayScreen(GameState):
 		self.side_up = None
 		
 	def start(self, players):
-		#TODO Uncomment lines 69-85 to setup player start values
+		#TODO: Uncomment Lines 69-85 to set up the player start values
 		#self.players = players
 		#self.player1 = players["player1"]
 		#self.player2 = players["player2"]
